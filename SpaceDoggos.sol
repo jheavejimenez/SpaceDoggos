30886f3026787067463eb59c31d92443410a6c6d
Jheave <jimenezjheave123@gmail.com>
2021-09-21 14:22:19 +0800

    first commit

---
+++ SpaceDoggos.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.20;
+
+contract SpaceDoggos {
+
+    uint maxPlanetsPerSystem = 10;
+    uint minPlanetsPerSystem = 3;
+
+    uint planetCodeDigits = 7;
+    uint systemCodeDigits = 7;
+
+    uint planetCodeModulus = 10 ** planetCodeDigits;
+    uint systemCodeModulus = 10 ** systemCodeDigits;
+
+    struct Doggo {
+        string name;
+        uint8 breed;
+        uint8 color;
+        uint8 face;
+        uint8 costume;
+        uint coordX;
+        uint coordY;
+    }
+
+    mapping(address => Doggo) doggos;
+
+    // Write your function below
+
+
+
+}
